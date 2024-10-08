cf5711343ec0bd15963d799591e71c65ab67caf5
Sajidedix <sajideditz27@gmail.com>
2024-10-09 04:25:04 +0530

    Update metadata.py

--- plugins/metadata.py
+++ plugins/metadata.py
@@ -6,10 +6,10 @@ from config import Txt
 
 
 
-ON = [[InlineKeyboardButton('Metadata On ✅', callback_data='metadata_1')], [
-    InlineKeyboardButton('Set Custom Metadata', callback_data='cutom_metadata')]]
-OFF = [[InlineKeyboardButton('Metadata Off ❌', callback_data='metadata_0')], [
-    InlineKeyboardButton('Set Custom Metadata', callback_data='cutom_metadata')]]
+ON = [[InlineKeyboardButton('Mᴇᴛᴀᴅᴀᴛᴀ Nɴ ✅', callback_data='metadata_1')], [
+    InlineKeyboardButton('Sᴇᴛ Cᴜsᴛᴏᴍ Mᴇᴛᴀᴅᴀᴛᴀ', callback_data='cutom_metadata')]]
+OFF = [[InlineKeyboardButton('Mᴇᴛᴀᴅᴀᴛᴀ Oғғ ❌', callback_data='metadata_0')], [
+    InlineKeyboardButton('Sᴇᴛ Cᴜsᴛᴏᴍ Mᴇᴛᴀᴅᴀᴛᴀ', callback_data='cutom_metadata')]]
 
 
 @Client.on_message(filters.private & filters.command('metadata'))
@@ -20,8 +20,8 @@ async def handle_metadata(bot: Client, message: Message):
     user_metadata = await jishubotz.get_metadata_code(message.from_user.id)
     await ms.delete()
     if bool_metadata:
-        return await message.reply_text(f"**Your Current Metadata :-**\n\n➜ `{user_metadata}` ",quote=True, reply_markup=InlineKeyboardMarkup(ON))
-    return await message.reply_text(f"**Your Current Metadata :-**\n\n➜ `{user_metadata}` ",quote=True, reply_markup=InlineKeyboardMarkup(OFF))
+        return await message.reply_text(f"**Yᴏᴜʀ Cᴜʀʀᴇɴᴛ Mᴇᴛᴀᴅᴀᴛᴀ :-**\n\n➜ `{user_metadata}` ",quote=True, reply_markup=InlineKeyboardMarkup(ON))
+    return await message.reply_text(f"**Yᴏᴜʀ Cᴜʀʀᴇɴᴛ Mᴇᴛᴀᴅᴀᴛᴀ :-**\n\n➜ `{user_metadata}` ",quote=True, reply_markup=InlineKeyboardMarkup(OFF))
 
 
 @Client.on_callback_query(filters.regex('.*?(custom_metadata|metadata).*?'))
@@ -35,11 +35,11 @@ async def query_metadata(bot: Client, query: CallbackQuery):
 
         if bool(eval(_bool)):
             await jishubotz.set_metadata(query.from_user.id, bool_meta=False)
-            await query.message.edit(f"**Your Current Metadata :-**\n\n➜ `{user_metadata}` ", reply_markup=InlineKeyboardMarkup(OFF))
+            await query.message.edit(f"**Yᴏᴜʀ Cᴜʀʀᴇɴᴛ Mᴇᴛᴀᴅᴀᴛᴀ :-**\n\n➜ `{user_metadata}` ", reply_markup=InlineKeyboardMarkup(OFF))
 
         else:
             await jishubotz.set_metadata(query.from_user.id, bool_meta=True)
-            await query.message.edit(f"**Your Current Metadata :-**\n\n➜ `{user_metadata}` ", reply_markup=InlineKeyboardMarkup(ON))
+            await query.message.edit(f"**Yᴏᴜʀ Cᴜʀʀᴇɴᴛ Mᴇᴛᴀᴅᴀᴛᴀ :-**\n\n➜ `{user_metadata}` ", reply_markup=InlineKeyboardMarkup(ON))
 
     elif data == 'cutom_metadata':
         await query.message.delete()
@@ -47,12 +47,12 @@ async def query_metadata(bot: Client, query: CallbackQuery):
             try:
                 metadata = await bot.ask(text=Txt.SEND_METADATA, chat_id=query.from_user.id, filters=filters.text, timeout=30, disable_web_page_preview=True, reply_to_message_id=query.message.id)
             except ListenerTimeout:
-                await query.message.reply_text("⚠️ Error !!\n\n**Request Timed Out.**\n\nRestart By Using /metadata", reply_to_message_id=query.message.id)
+                await query.message.reply_text("⚠️ Eʀʀᴏʀ !!\n\n** Rᴇǫᴜᴇsᴛ Tɪᴍᴇᴅ Oᴜᴛ.**\n\nRestart By Using /metadata", reply_to_message_id=query.message.id)
                 return
             print(metadata.text)
             ms = await query.message.reply_text("**Please Wait...**", reply_to_message_id=metadata.id)
             await jishubotz.set_metadata_code(query.from_user.id, metadata_code=metadata.text)
-            await ms.edit("**Your Metadata Code Set Successfully ✅**")
+            await ms.edit("**Yᴏᴜʀ Mᴇᴛᴀᴅᴀᴛᴀ Cᴏᴅᴇ Sᴇᴛ Sᴜᴄᴄᴇssғᴜʟʟy ✅**")
         except Exception as e:
             print(e)
 
