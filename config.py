a0e9239b80c116693002d221d3b9ee5884b2f373
Sajidedix <sajideditz27@gmail.com>
2025-06-06 03:52:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config(object):
     API_ID    = os.environ.get("API_ID", "10658015")
     API_HASH  = os.environ.get("API_HASH", "a0087bca748f86698c53d291c9e5b3af")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "7981857738:AAFvSoagF7lrOHNY-UVqyCE3kXLRk4Kqj2Y") 
-   
+
     # database config
     DB_NAME = os.environ.get("DB_NAME","Cluster0")     
     DB_URL  = os.environ.get("DB_URL","mongodb+srv://helphm9:MO7vlO8DtXqezh3i@cluster0.mom8p.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
@@ -16,7 +16,7 @@ class Config(object):
     # other configs
     BOT_UPTIME  = time.time()
     START_PIC   = os.environ.get("START_PIC", "https://envs.sh/gNW.jpg")
-    ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '6170962395').split()]
+    ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '7657218453').split()]
 
     # channels logs
     FORCE_SUB   = os.environ.get("FORCE_SUB", "SandVillage") 
